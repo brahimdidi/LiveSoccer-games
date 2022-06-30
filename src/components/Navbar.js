a0d71189a494b39c8574bd0d96f812f00e172949
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-30 19:59:17 +0000

    create navbar to allow user browse app pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../images/liveScoreLogo.png';
+// import './Navbar.module.css';
+
+export default function Header() {
+  const navStyles = ({ isActive }) => ({
+    fontWeight: isActive ? 'bold' : 'normal',
+    background: isActive ? 'blue' : 'none',
+  });
+
+  return (
+    <header>
+      <div className="header-logo-and-name">
+        <img className="header-logo" src={logo} alt="planet logo" />
+        <h1>liveScore</h1>
+      </div>
+      <nav>
+        <NavLink className="navLink" style={navStyles} to="/">Games</NavLink>
+        <NavLink className="navLink" style={navStyles} to="/Competition">Competition</NavLink>
+      </nav>
+    </header>
+  );
+}
