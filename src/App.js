75b2c5033f5e2aa1beaf54b4d1f8f604c7f410bf
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-30 19:55:55 +0000

    use react router add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import AllGames from './components/AllGames';
+import Competition from './components/Competition';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<AllGames />} />
+      <Route path="/Competition" element={<Competition />} />
+    </Routes>
+  </div>
+);
 
 export default App;
